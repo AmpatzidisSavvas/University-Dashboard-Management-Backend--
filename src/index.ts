38e073d8fdb045bd3fff032bface6cb34045b577
AmpatzidisSavvas <savvas.ampa@gmail.com>
2026-01-21 09:57:47 +0200

    Implement better auth

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import subjectRouter from './routes/subjects';
 import cors from 'cors'
 import securityMiddleware from './middleware/security';
+import { toNodeHandler } from "better-auth/node"
+import { auth } from './lib/auth';
 
 const app = express();
 const PORT = 8000;
@@ -14,7 +16,9 @@ app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET','POST','PUT','DELETE'],
     credentials: true 
-}))
+}));
+
+app.all('/api/auth/{*splat}', toNodeHandler(auth));
 
 app.use(express.json());
 
